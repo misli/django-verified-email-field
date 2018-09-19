9c7c122b92cd5dc3330f8c7faa67b78f46a2cdcb
Jakub Dorňák <jakub.dornak@misli.cz>
2018-09-19 14:04:05 +0200

    fix url pattern to accept fieldsetup_id with letters

--- verified_email_field/urls.py
+++ verified_email_field/urls.py
@@ -7,5 +7,5 @@ from . import views
 app_name = 'verified-email-field'
 
 urlpatterns = [
-    url(r'^send/(?P<fieldsetup_id>\w+)/$', views.send, name='send'),
+    url(r'^send/(?P<fieldsetup_id>[^/]+)/$', views.send, name='send'),
 ]
