088555d48aaff764fff9a186b8c11014c05a360f
Jakub Dorňák <jakub.dornak@misli.cz>
2020-01-16 22:35:11 +0100

    Drop import unicode_literals (Python 2 support)

--- verified_email_field/widgets.py
+++ verified_email_field/widgets.py
@@ -1,5 +1,3 @@
-from __future__ import unicode_literals
-
 from django import forms
 from django.forms.widgets import EmailInput, MultiWidget, TextInput
 from django.templatetags.static import static
