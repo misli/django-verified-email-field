4b1794bff3d1c66a3faf19e1878d8d5cbef9769e
Sumanth Ratna <sumanthratna@gmail.com>
2020-07-20 10:59:03 -0400

    Use Ajax if it is available
    Ajax may be less buggy than XMLHttpRequest.

--- verified_email_field/static/verified_email_field/send.js
+++ verified_email_field/static/verified_email_field/send.js
@@ -16,13 +16,29 @@ function getCookie(name) {
 
 
 function send_verification_code(id, url, message) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            if (xhttp.responseText) alert(xhttp.responseText);
-            document.getElementById(id.replace('_0', '_1')).focus();
-        }
-    };
-    xhttp.open("POST", url, true);
-    xhttp.send(`csrfmiddlewaretoken=${getCookie('csrftoken')}&email=${document.getElementById(id).value}`);
+    if (window.jQuery) {
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: {
+                csrfmiddlewaretoken: getCookie('csrftoken'),
+                email: document.getElementById(id).value,
+            },
+            success: function(msg) {
+                if (message) alert(message);
+                document.getElementById(id.replace('_0', '_1')).focus();
+            },
+        });
+    }
+    else {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                if (xhttp.responseText) alert(xhttp.responseText);
+                document.getElementById(id.replace('_0', '_1')).focus();
+            }
+        };
+        xhttp.open("POST", url, true);
+        xhttp.send(`csrfmiddlewaretoken=${getCookie('csrftoken')}&email=${document.getElementById(id).value}`);
+    }
 }
