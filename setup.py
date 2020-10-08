40adad07e7a4ea49263e162338743e5a666ed43d
Jakub Dorňák <jakub.dornak@misli.cz>
2020-10-08 16:05:18 +0200

    Bump version: 1.6.1 → 1.7.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name            = 'django-verified-email-field',
-    version         = '1.6.1',
+    version         = '1.7.0',
     description     = 'Simple model and form field to get verified email',
     author          = 'Jakub Dorňák',
     author_email    = 'jakub.dornak@misli.cz',
