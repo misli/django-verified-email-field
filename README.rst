a7222e4c40dcb523cc619bd3631bf04a96eb65d2
Jakub Dorňák <jakub.dornak@qbsoftware.cz>
2025-03-27 11:12:01 +0100

    Update setup.py

---
+++ README.rst
@@ -0,0 +1,68 @@
+django-verified-email-field
+===========================
+
+Simple model and form field to get verified email
+
+Renders two input fields: ``e-mail`` and ``verification code``. The
+verification code is send to the e-mail address using AJAX or during
+field’s ``clean``, if there is no valid code for given e-mail, so it
+works even without javascript. For the javascript to work properly You
+need to have `jQuery <https://code.jquery.com/>`__ included in Your
+page.
+
+All the texts and email templates may be configured in ``settings`` and
+overidden by field’s keyword arguments. (See
+`settings.py <verified_email_field/settings.py>`__ and
+`fieldsetup.py <verified_email_field/fieldsetup.py>`__ for more
+information)
+
+Installation
+------------
+
+.. code:: bash
+
+   pip install django-verified-email-field
+
+Configuration
+-------------
+
+1. add ``'verified_email_field'`` to Your ``settings.INSTALLED_APPS``
+2. include ``verified_email_field.urls`` in your project’s urls.py using
+   namespace ``'verified-email-field'``:
+
+.. code:: python
+
+   from django.conf.urls import url, include
+
+   urlpatterns = [
+       ...
+       url(r'^verified-email-field/', include('verified_email_field.urls')),
+       ...
+   ]
+
+Usage
+-----
+
+Use ``VerifiedEmailField`` in Your forms:
+
+.. code:: python
+
+   from django import forms
+   from verified_email_field.forms import VerifiedEmailField
+
+   class RegistrationForm(forms.ModelForm):
+       email = VerifiedEmailField(label='email', fieldsetup_id='registration-form-email', required=True)
+
+Or in Your models:
+
+.. code:: python
+
+   from django.db import models
+   from verified_email_field.models import VerifiedEmailField
+
+   class User(models.Model):
+       email = VerifiedEmailField('e-mail', fieldsetup_id='user-email')
+
+Ensure that ``form.media.js`` (``{{ form.media.js }}``) is being
+rendered in your template and `jQuery <https://code.jquery.com/>`__ is
+included in Your page.
--- setup.py
+++ setup.py
@@ -1,33 +1,34 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
+import os
 from setuptools import setup, find_packages
 
+with open(os.path.join(os.path.dirname(__file__), "README.rst")) as readme:
+    long_description = readme.read()
+
 setup(
-    name            = 'django-verified-email-field',
-    version         = '1.10.0',
-    description     = 'Simple model and form field to get verified email',
-    author          = 'Jakub Dorňák',
-    author_email    = 'jakub.dornak@misli.cz',
-    license         = 'BSD',
-    url             = 'https://github.com/misli/django-verified-email-field',
-    packages        = find_packages(),
-    include_package_data = True,
-    install_requires = [
-    ],
-    classifiers     = [
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Framework :: Django :: 1.11',
-        'Framework :: Django :: 2.1',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Natural Language :: Czech',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+    name="django-verified-email-field",
+    version="1.10.0",
+    description="Simple model and form field to get verified email",
+    long_description=long_description,
+    author="Jakub Dorňák",
+    author_email="jakub.dornak@qbsoftware.cz",
+    license="BSD",
+    url="https://github.com/misli/django-verified-email-field",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 2",
+        "Framework :: Django :: 3",
+        "Framework :: Django :: 4",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: Czech",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
