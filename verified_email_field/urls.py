464f257241ac22eac0a175561111fef8acd7b5b8
Jakub Dorňák <jakub.dornak@qbsoftware.cz>
2025-03-27 09:51:15 +0100

    Support Django 4

--- verified_email_field/urls.py
+++ verified_email_field/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 app_name = "verified-email-field"
 
 urlpatterns = [
-    url(r"^send/(?P<fieldsetup_id>[^/]+)/$", views.send, name="send"),
+    path("send/<fieldsetup_id>/", views.send, name="send"),
 ]
